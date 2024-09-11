c13b84eafee96dd1c212ecaa99dfd33c363e48ab
DukeRadoc <qdgjaradoc@tip.edu.ph>
2024-09-11 11:55:32 +0800

    Add files via upload

---
+++ Excercise3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int x=0, y=0;
+		
+		cout<<"Input X value :";
+		cin>>x ;
+		cout<<"Input Y value :";
+		cin>>y ;
+		
+		if(x>y)
+		{cout<<"X is greater than Y";}
+		else if(x<y)
+		{cout<<"Y is greater than X";}
+		else
+		{cout<<"X and Y are equals";}
+
+	return 0;
+}
