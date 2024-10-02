ad1ac8b91ef4eb2359a2b191bdf93fd5da081d1a
DukeRadoc <qdgjaradoc@tip.edu.ph>
2024-10-02 12:51:43 +0800

    Add files via upload

---
+++ PracticalTest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a=0, b=5, c=0, d=0;
+
+while(a<10)
+{
+b=b-1;
+c=b+b;
+cout<<b<<" Is Lesser Than 5"<<endl;
+a++;
+};
+cout<<"The Sum Of All Of them is : "<<c;
+
+return 0;
+}
